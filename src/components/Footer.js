c6af901a0b50ff2fb012b200fb1839ad5d0c5831
csharma05 <78192378+csharma05@users.noreply.github.com>
2023-02-15 19:50:24 +0530

    project uploaded

---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      {" "}
+      <div className="footer">
+        <div className="inner-footer">
+          <div className="footer-items">
+            <h2>Khushmeen's Ecommerce</h2>
+            <br />
+            <p>We are here to help people buy their loved items.</p>
+          </div>
+
+          <div className="footer-items">
+            <br />
+            <h3>Quick Links</h3>
+            <div className="border1"></div>
+            <ul className="foot-list">
+              <Link href={"/"} role="link">
+                <li className="cursor-pointer">Home</li>
+              </Link>
+              <Link href={"/cart"} role="link">
+                <li className="cursor-pointer">Cart</li>
+              </Link>
+              <Link href={"/orders"} role="link">
+                <li className="cursor-pointer">Orders</li>
+              </Link>
+            </ul>
+          </div>
+
+          <div className="footer-items">
+            <br />
+            <h3>Collaborators</h3>
+            <div className="border1"></div>
+            <ul className="foot-list">
+              <a href="#">
+                <li>Geetakshi</li>
+                <li>Kritika</li>
+                <li>Paridhi</li>
+              </a>
+            </ul>
+          </div>
+
+          <div className="footer-items">
+            <br />
+            <h3>Contact us</h3>
+            <div className="border1"></div>
+            <ul className="foot-list">
+              <li>Chitkara University</li>
+              <li>123456789</li>
+              <li>no-reply@amazon-clone.com</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer-bottom">
+          Made by Khushmeen Sharma with ❤️ and React(Next JS).
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
